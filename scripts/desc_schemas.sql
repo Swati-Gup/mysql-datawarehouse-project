c91cc16f3ad0c058dc0d09fd7a73bb453d5db5cf
swatig <43838605+Swati-Gup@users.noreply.github.com>
2025-04-01 22:14:48 +1100

    Create desc_schemas.sql

---
+++ scripts/desc_schemas.sql
@@ -0,0 +1,20 @@
+/** 
+	Script to check Bronze layer schema details
+**/
+
+DESCRIBE bronze.crm_cust_info;
+DESCRIBE bronze.crm_prd_info;
+DESCRIBE bronze.crm_sales_details;
+
+DESCRIBE bronze.erp_cust_az12;
+DESCRIBE bronze.erp_loc_a101;
+DESCRIBE bronze.erp_px_cat_g1v2;
+
+
+DESCRIBE silver.crm_cust_info;
+DESCRIBE silver.crm_prd_info;
+DESCRIBE silver.crm_sales_details;
+
+DESCRIBE silver.erp_cust_az12;
+DESCRIBE silver.erp_loc_a101;
+DESCRIBE silver.erp_px_cat_g1v2;
